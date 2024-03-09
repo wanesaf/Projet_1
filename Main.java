8766236f75ec5ec8b85c98915c8eda12707ef16e
wanesaf <safwanesouhil.dendani@etu.usthb.dz>
2024-03-09 13:47:02 +0100

    ajouter un nouveau abonné

--- Main.java
+++ Main.java
@@ -14,6 +14,8 @@ public class Main {
         Abonne abonne3 = new Abonne("Sami", "karim", "487946131", true);
         Abonne abonne4 = new Abonne("riyad", "kadiri", "787894865", true);
         Abonne abonne5 = new Abonne("wassim", "zakari", "48985220", false);
+       // Abonne abonne6 = new Abonne("dela3a","pin","54566",true);
+        
 
         //Creation des agents
         Agent agent1 = new Agent("Hamidi", "Hamid", "100000");
